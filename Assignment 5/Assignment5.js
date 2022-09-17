7b13c034c656495886ae39b1c0c8c03d49004fb3
Muhammad Mubashir Khan <50612566+Mubashir23944@users.noreply.github.com>
2022-09-17 14:57:10 +0500

    Assignment 5 completed

---
+++ Assignment 5/Assignment5.js
@@ -0,0 +1,40 @@
+// var age = prompt('Enter your age')
+
+// if(age >= 18){
+//     console.log('You are ready to proceed');
+// }else{
+//     console.log('Your are not allowed to proceed');
+// }
+
+// Example of Equal (==)
+var runs = 100;
+if(runs == 100){
+    console.log('Example of Equal to Equal to:','Your century is completed');
+}
+
+// Example of greater than (>)
+runs = 101;
+
+if(runs > 100){
+    console.log('Example of Greater than:','You are not out even after century');
+}
+
+// Example of less than (<)
+runs = 99;
+
+if(runs < 100){
+    console.log('Example of Less than:','You got unluckily out before your century');
+}
+
+// Example of Greater than equal to (>=)
+score = 200;
+
+if(score >= 200){
+    console.log('Example of Greater than equal to:','The score is tough to chase');
+}
+// Example of Less than equal to (<=)
+teenAge = 18;
+
+if(teenAge <= 18){
+    console.log('Example of Less than equal to:','You are in your teen age');
+}
