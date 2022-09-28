03d6da8e6306ab6288494ac8af1c099f2ebeb166
Muhammad Mubashir Khan <50612566+Mubashir23944@users.noreply.github.com>
2022-09-28 18:23:13 +0500

    Assignment 11 completed

---
+++ Assignment 11/index.js
@@ -0,0 +1,43 @@
+// Math.round()
+console.log('Example of Round of :','My total percentage is ' + Math.round(67.5));
+
+// Math.ceil()
+console.log('Example of Ceil :','My total percentage is ' + Math.ceil(67.5));
+
+// Math.floor()
+console.log('Example of Floor :','My total percentage is ' + Math.floor(67.5));
+
+// Math.random()
+console.log('Example of Random :','Your OTP Code is  ' + Math.random());
+
+// parseInt()
+var prevAge = '26';
+var currentAge = parseInt(prevAge) + 1
+console.log('Example of parseInt :','My age is is  ' + currentAge);
+
+// parseFloat()
+var prevResult = '75.3'
+var improvedResult = parseFloat(prevResult) + 10
+console.log('Example of parseFloat :','My total percentage is is  ' + improvedResult);
+
+// toString()
+var number = 89;
+var result = number.toString() + 23
+console.log('Example of toString :', result);
+
+// Number()
+var numberFunctionResult = Number(prevResult) + 10
+console.log('Example of Number :','My total percentage is is  ' + numberFunctionResult);
+
+// toFixed()
+var num = 78.35;
+var n = num.toFixed();
+console.log('Example of toFixed :', n);
+
+// toUpperCase()
+var username = 'ali'
+console.log('Example of toUpperCase :','Username is  ' + username.toUpperCase());
+
+// toLowerCase()
+var user = 'ALI'
+console.log('Example of toLowerCase :','Username is  ' + username.toLowerCase());
